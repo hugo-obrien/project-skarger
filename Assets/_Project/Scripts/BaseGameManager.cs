665eeb1eb39cd47fb34406e873f320d4c5e7ff49
Sergey Gubenko <sa.gubenko@gmail.com>
2026-08-13 11:58:04 +0300

    initial unity project

---
+++ Assets/_Project/Scripts/BaseGameManager.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class BaseGameManger : MonoBehaviour
+{
+   private void Awake() {
+      print("Welcome to the world of Skarger");
+      print("Brace yourself to epic adventures (or not)");
+   }
+}
