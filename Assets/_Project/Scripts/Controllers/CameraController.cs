a0a5f7206c4395bb3062006f819d8af415663919
Hugo-Obrien <sa.gubenko@gmail.com>
2026-09-17 17:26:30 +0300

    small cleanup

--- Assets/_Project/Scripts/Controllers/CameraController.cs
+++ Assets/_Project/Scripts/Controllers/CameraController.cs
@@ -173,7 +173,7 @@ namespace _Project.Scripts.Controllers
 
             if (!disableEdgeScrollWhileMouseRotating || !mouseRotationActive)
             {
-                // moveInput += GetEdgeScrollInput(); todo rollback
+                moveInput += GetEdgeScrollInput();
             }
 
             moveInput = Vector2.ClampMagnitude(moveInput, MaxInputMagnitude);
--- Assets/_Project/Scripts/Controllers/UnitRightClickHandler.cs
+++
@@ -1,7 +0,0 @@
-namespace _Project.Scripts.Controllers
-{
-    public class UnitRightClickHandler
-    {
-        
-    }
-}
